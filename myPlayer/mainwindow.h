b1d912c6a49e8aae32795f6cc21e87a59d4ae1b3
sc19hz <80456998+sc19hz@users.noreply.github.com>
2021-12-17 16:00:48 +0800

    cycle2.1
    features：
    play
    stop
    fullscreen(some problem)
    list
    volumn

---
+++ myPlayer/mainwindow.h
@@ -0,0 +1,63 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QFileDialog>
+#include <QMediaPlaylist>
+#include <QMediaPlayer>
+#include "video.h"
+#include "the_button.h"
+
+namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+private slots:
+    void on_pushButton_4_clicked(bool checked); // open video file
+
+    void on_pushButton_2_clicked(bool checked); // start play media
+
+    void on_pushButton_3_clicked(bool checked); // pause video
+
+    void on_pushButton_pressed(); // full screen
+
+    void onTimerOut();
+
+    void slider_process_clicked();
+
+    void slider_process_moved();
+
+    void slider_process_released();
+
+    void on_slider_process_sliderMoved();
+
+    void on_slider_process_sliderReleased();
+
+//    void keyPressEvent(QKeyEvent *keyset);
+
+    void on_thebutton_1_clicked(bool checked);
+
+    void on_thebutton_2_clicked(bool checked);
+
+private:
+    Ui::MainWindow *ui;
+    QString currentMediaFileName;
+    QMediaPlayer *play;
+    QMediaPlaylist *playList;
+
+    QMediaPlayer *play_1;
+    QMediaPlaylist *playList_1;
+    QMediaPlayer *play_2;
+    QMediaPlaylist *playList_2;
+    std::vector<TheButtonInfo> videos;
+};
+
+#endif // MAINWINDOW_H
---
+++ myPlayer/moc_mainwindow.cpp
@@ -0,0 +1,165 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'mainwindow.h'
+**
+** Created by: The Qt Meta Object Compiler version 67 (Qt 5.9.7)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include "mainwindow.h"
+#include <QtCore/qbytearray.h>
+#include <QtCore/qmetatype.h>
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'mainwindow.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 67
+#error "This file was generated using the moc from 5.9.7. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+QT_BEGIN_MOC_NAMESPACE
+QT_WARNING_PUSH
+QT_WARNING_DISABLE_DEPRECATED
+struct qt_meta_stringdata_MainWindow_t {
+    QByteArrayData data[15];
+    char stringdata0[302];
+};
+#define QT_MOC_LITERAL(idx, ofs, len) \
+    Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
+    qptrdiff(offsetof(qt_meta_stringdata_MainWindow_t, stringdata0) + ofs \
+        - idx * sizeof(QByteArrayData)) \
+    )
+static const qt_meta_stringdata_MainWindow_t qt_meta_stringdata_MainWindow = {
+    {
+QT_MOC_LITERAL(0, 0, 10), // "MainWindow"
+QT_MOC_LITERAL(1, 11, 23), // "on_pushButton_4_clicked"
+QT_MOC_LITERAL(2, 35, 0), // ""
+QT_MOC_LITERAL(3, 36, 7), // "checked"
+QT_MOC_LITERAL(4, 44, 23), // "on_pushButton_2_clicked"
+QT_MOC_LITERAL(5, 68, 23), // "on_pushButton_3_clicked"
+QT_MOC_LITERAL(6, 92, 21), // "on_pushButton_pressed"
+QT_MOC_LITERAL(7, 114, 10), // "onTimerOut"
+QT_MOC_LITERAL(8, 125, 22), // "slider_process_clicked"
+QT_MOC_LITERAL(9, 148, 20), // "slider_process_moved"
+QT_MOC_LITERAL(10, 169, 23), // "slider_process_released"
+QT_MOC_LITERAL(11, 193, 29), // "on_slider_process_sliderMoved"
+QT_MOC_LITERAL(12, 223, 32), // "on_slider_process_sliderReleased"
+QT_MOC_LITERAL(13, 256, 22), // "on_thebutton_1_clicked"
+QT_MOC_LITERAL(14, 279, 22) // "on_thebutton_2_clicked"
+
+    },
+    "MainWindow\0on_pushButton_4_clicked\0\0"
+    "checked\0on_pushButton_2_clicked\0"
+    "on_pushButton_3_clicked\0on_pushButton_pressed\0"
+    "onTimerOut\0slider_process_clicked\0"
+    "slider_process_moved\0slider_process_released\0"
+    "on_slider_process_sliderMoved\0"
+    "on_slider_process_sliderReleased\0"
+    "on_thebutton_1_clicked\0on_thebutton_2_clicked"
+};
+#undef QT_MOC_LITERAL
+
+static const uint qt_meta_data_MainWindow[] = {
+
+ // content:
+       7,       // revision
+       0,       // classname
+       0,    0, // classinfo
+      12,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       0,       // signalCount
+
+ // slots: name, argc, parameters, tag, flags
+       1,    1,   74,    2, 0x08 /* Private */,
+       4,    1,   77,    2, 0x08 /* Private */,
+       5,    1,   80,    2, 0x08 /* Private */,
+       6,    0,   83,    2, 0x08 /* Private */,
+       7,    0,   84,    2, 0x08 /* Private */,
+       8,    0,   85,    2, 0x08 /* Private */,
+       9,    0,   86,    2, 0x08 /* Private */,
+      10,    0,   87,    2, 0x08 /* Private */,
+      11,    0,   88,    2, 0x08 /* Private */,
+      12,    0,   89,    2, 0x08 /* Private */,
+      13,    1,   90,    2, 0x08 /* Private */,
+      14,    1,   93,    2, 0x08 /* Private */,
+
+ // slots: parameters
+    QMetaType::Void, QMetaType::Bool,    3,
+    QMetaType::Void, QMetaType::Bool,    3,
+    QMetaType::Void, QMetaType::Bool,    3,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void, QMetaType::Bool,    3,
+    QMetaType::Void, QMetaType::Bool,    3,
+
+       0        // eod
+};
+
+void MainWindow::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        MainWindow *_t = static_cast<MainWindow *>(_o);
+        Q_UNUSED(_t)
+        switch (_id) {
+        case 0: _t->on_pushButton_4_clicked((*reinterpret_cast< bool(*)>(_a[1]))); break;
+        case 1: _t->on_pushButton_2_clicked((*reinterpret_cast< bool(*)>(_a[1]))); break;
+        case 2: _t->on_pushButton_3_clicked((*reinterpret_cast< bool(*)>(_a[1]))); break;
+        case 3: _t->on_pushButton_pressed(); break;
+        case 4: _t->onTimerOut(); break;
+        case 5: _t->slider_process_clicked(); break;
+        case 6: _t->slider_process_moved(); break;
+        case 7: _t->slider_process_released(); break;
+        case 8: _t->on_slider_process_sliderMoved(); break;
+        case 9: _t->on_slider_process_sliderReleased(); break;
+        case 10: _t->on_thebutton_1_clicked((*reinterpret_cast< bool(*)>(_a[1]))); break;
+        case 11: _t->on_thebutton_2_clicked((*reinterpret_cast< bool(*)>(_a[1]))); break;
+        default: ;
+        }
+    }
+}
+
+const QMetaObject MainWindow::staticMetaObject = {
+    { &QMainWindow::staticMetaObject, qt_meta_stringdata_MainWindow.data,
+      qt_meta_data_MainWindow,  qt_static_metacall, nullptr, nullptr}
+};
+
+
+const QMetaObject *MainWindow::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *MainWindow::qt_metacast(const char *_clname)
+{
+    if (!_clname) return nullptr;
+    if (!strcmp(_clname, qt_meta_stringdata_MainWindow.stringdata0))
+        return static_cast<void*>(this);
+    return QMainWindow::qt_metacast(_clname);
+}
+
+int MainWindow::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QMainWindow::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 12)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 12;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 12)
+            *reinterpret_cast<int*>(_a[0]) = -1;
+        _id -= 12;
+    }
+    return _id;
+}
+QT_WARNING_POP
+QT_END_MOC_NAMESPACE
